417516ad26c333a870768e7617c99368fe05b17d
Israr8957 <ik6141333@gmail.com>
2026-07-17 14:40:34 +0530

    file changes

--- app.js
+++ app.js
@@ -8,13 +8,25 @@ const ticketRoutes = require("./routes/ticket.route.js");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "SupportSphere CRM Backend is Running 🚀",
+  });
+});
 
 app.use("/api/test", testRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/tickets", ticketRoutes);
-
 
 module.exports = app;
